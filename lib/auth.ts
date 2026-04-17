18c5683b4c10af8863ee87add571a377184a57e6
nemealvaro <alvaronemers2001@gmail.com>
2026-04-17 22:23:20 +0200

    prisma changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,9 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as NextAuthOptions["adapter"],
   session: {
     strategy: "jwt",
   },
